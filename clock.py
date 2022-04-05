21be584bdcd65c219908aca939905e23eff167df
Anna <klimova_88@mail.ru>
2022-04-05 12:50:38 +0300

    shred

--- clock.py
+++ clock.py
@@ -1,8 +1,6 @@
-from apscheduler.schedulers.blocking import BlockingScheduler
-
 from src.main import get_weather
-
-
+from apscheduler.schedulers.blocking import BlockingScheduler
+sched = BlockingScheduler()
 @sched.scheduled_job('cron', day_of_week='mon-sun', hour=6)
 def scheduled_job():
     print('Start clock')
